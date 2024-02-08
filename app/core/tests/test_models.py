a9713352a1bd1164be8d6cc651632bfdf843d418
Khwilo Kabaka <khwilowatai@gmail.com>
2024-02-08 13:29:03 +0300

    feat: check for empty email address

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,3 +35,8 @@ class ModelTests(TestCase):
                 password='sample123'
             )
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises an exception"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'pass123')
