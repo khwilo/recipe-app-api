cd24cebea35cfa9e691ede643ea0579c823db85e
Khwilo Kabaka <khwilowatai@gmail.com>
2024-08-21 23:32:14 +0300

    feat: implement ingredient listing API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,7 +6,7 @@ from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Ingredient, Recipe, Tag
 from recipe import serializers
 
 
@@ -49,4 +49,17 @@ class TagViewSet(
 
     def get_queryset(self):
         """Retrieve tags for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database"""
+
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve ingredients for authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
